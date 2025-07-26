372b4021b98b2a8593882b0a163cc49448895967
amrali373 <hippax85@gmail.com>
2025-07-27 00:31:15 +0330

    edit

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,17 +1,16 @@
-import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-import { RegisterComponent } from './components/register/register.component';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet
-  ,RegisterComponent
+  standalone: true,
+  imports: [
+    RouterOutlet, RouterModule,
+    MatButtonModule
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  http = inject(HttpClient);
 }
